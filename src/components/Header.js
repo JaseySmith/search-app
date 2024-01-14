c4c652f4272da7ccf8b697efa8572ac16285af6d
Jasey Smith <jaseyhsmith@gmail.com>
2024-01-13 18:01:22 -0800

    Change background when scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DarkModeToggle from './DarkMode';
 
 export default function Header() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
-  
+  const [isScrolled, setIsScrolled] = useState(false);
+
   const handleNavClick = () => {
     setIsNavExpanded(!isNavExpanded);
   };
 
+  const handleScroll = () => {
+    setIsScrolled(window.scrollY > 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div id="header">
+    <div id="header" className={isScrolled ? 'scrolled' : ''}>
       <div className="container">
         {/* Logo */}
         <div>
